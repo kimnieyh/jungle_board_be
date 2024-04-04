0739b02fb1c07ef036259e1e3abbebb75447dc05
hyeinKim <kimnieyh@gmail.com>
2024-04-04 15:26:27 +0900

    init

---
+++ src/main/java/com/example/demo/JungleBoardBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungleBoardBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JungleBoardBeApplication.class, args);
+	}
+
+}
